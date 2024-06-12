8989251509d052e130585f09e1dacd0fa76501f1
Zorica Vuković <90617418+zoricavukovic@users.noreply.github.com>
2024-06-12 18:51:38 +0200

    fix: add kafka configuration to kube env (#8)

--- auth-service/infrastructure/dto/user_dto.go
+++ auth-service/infrastructure/dto/user_dto.go
@@ -1,10 +1,11 @@
 package dto
 
 type UserDTO struct {
-	Id            string `json:"sub"`
-	Email         string `json:"email"`
-	GivenName     string `json:"given_name"`
-	FamilyName    string `json:"family_name"`
-	Address       string `json:"address"`
-	EmailVerified bool   `json:"email_verified"`
+	Id            string   `json:"sub"`
+	Email         string   `json:"email"`
+	GivenName     string   `json:"given_name"`
+	FamilyName    string   `json:"family_name"`
+	Address       string   `json:"address"`
+	EmailVerified bool     `json:"email_verified"`
+	Role          []string `json:"role"`
 }
